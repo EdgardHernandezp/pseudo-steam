07bfc290be63c5656773caaaf2423df90ab9c5f5
Edgar Hernandez <edgardhernandezp@gmail.com>
2025-05-22 18:05:41 -0400

    Configured S3 client and implemented the bucket creation

---
+++ src/main/java/com/dreamseeker/pseudo_steam/services/AWSObjectStorageClient.java
@@ -0,0 +1,49 @@
+package com.dreamseeker.pseudo_steam.services;
+
+import com.dreamseeker.pseudo_steam.domains.BucketsPage;
+import com.dreamseeker.pseudo_steam.exceptions.BucketDoesNotExistException;
+import com.dreamseeker.pseudo_steam.exceptions.BucketNameExistsException;
+import com.dreamseeker.pseudo_steam.exceptions.BucketNotEmptyException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.BucketAlreadyExistsException;
+import software.amazon.awssdk.services.s3.model.BucketAlreadyOwnedByYouException;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class AWSObjectStorageClient implements ObjectStorageClient {
+
+    private final S3Client s3Client;
+
+    @Override
+    public BucketsPage.Bucket createBucket(String bucketName) throws BucketNameExistsException {
+        try {
+            String newBucketName = String.format("dev.%s-%s", bucketName, UUID.randomUUID());
+            CreateBucketRequest createBucketRequest = CreateBucketRequest.builder()
+                    .bucket(newBucketName)
+                    .build();
+            s3Client.createBucket(createBucketRequest);
+            return new BucketsPage.Bucket(newBucketName, Instant.now());
+        } catch (BucketAlreadyExistsException | BucketAlreadyOwnedByYouException e) {
+            log.error("Bucket ({}) already exists", bucketName, e);
+            throw new BucketNameExistsException(bucketName, e);
+        }
+    }
+
+    @Override
+    public BucketsPage fetchBuckets(Integer limit, String continuationToken) {
+        return null;
+    }
+
+    @Override
+    public void deleteBucket(String bucketName) throws BucketDoesNotExistException, BucketNotEmptyException {
+
+    }
+}
